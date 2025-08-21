ec6a942b135e3bfb21618accc91a186ea0093785
William George <williamgeorge1997x@gmail.com>
2025-08-21 19:12:36 +0300

    update

--- Modules/User/DTO/UserDto.php
+++ Modules/User/DTO/UserDto.php
@@ -11,20 +11,27 @@ class UserDto
     public $phone;
     public $password;
 
-    public function __construct($request) {
-        if($request->get('country_code')) $this->country_code = $request->get('country_code');
-        if($request->get('phone')) $this->phone = $request->get('phone');
-        if($request->get('password')) $this->password = Hash::make($request->get('password'));
+    public function __construct($request)
+    {
+        if ($request->get('country_code'))
+            $this->country_code = $request->get('country_code');
+        if ($request->get('phone'))
+            $this->phone = $request->get('phone');
+        if ($request->get('password'))
+            $this->password = Hash::make($request->get('password'));
     }
 
     public function dataFromRequest()
     {
-        $data =  json_decode(json_encode($this), true);
-        if($this->country_code == null) unset($data['country_code']);
-        if($this->phone == null) unset($data['phone']);
-        if($this->password == null) unset($data['password']);
-        $data['verify_code'] = rand(1000,9999);
-        // $data['verify_code'] = 9999;
+        $data = json_decode(json_encode($this), true);
+        if ($this->country_code == null)
+            unset($data['country_code']);
+        if ($this->phone == null)
+            unset($data['phone']);
+        if ($this->password == null)
+            unset($data['password']);
+        // $data['verify_code'] = rand(1000,9999);
+        $data['verify_code'] = 9999;
         return $data;
     }
 }
